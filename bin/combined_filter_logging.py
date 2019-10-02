6d8a5d9d4544e4aa156a6bab126bc0c1dd0e4ef9
Troy Rowan <tnr343@MUgenomics04.col.missouri.edu>
2019-10-02 06:38:33 -0500

    added hwe filter to logging script

--- bin/combined_filter_logging.py
+++ bin/combined_filter_logging.py
@@ -49,22 +49,22 @@ for xx in filenames:
                     print("!!HOLY SHIT WE DROPPED TOO MUCH!!\n")
                 else:
                     print("Individual Filtering okay\n")
-    # with open(hwefile, "r") as hwe:
-    #     for line in hwe:
-    #         if "variants loaded from .bim file" in line:
-    #             start_snps = line.split()[0]
-    #             print("HWE_Filtering")
-    #             print("StartingSNPs:\t", start_snps)
-    #         if "removed due to Hardy-Weinberg exact test." in line:
-    #             rem_snps = line.split()[1]
-    #             end_snps = int(start_snps) - int(rem_snps)
-    #             proportion = int(rem_snps)/int(start_snps)
-    #             percent = str(round(int(rem_snps)/int(start_snps)*100, 3))
-    #             print("RemovedSNPs:\t" + rem_snps)
-    #             print("EndSNPs:\t" + str(end_snps))
-    #             print("PercentSNPRemoved:\t" + percent + "%")
-    #             #print(proportion)
-    #             if proportion > 0.1:
-    #                 print("!!HOLY SHIT WE DROPPED TOO MUCH!!\n\n")
-    #             else:
-    #                 print("HWE Filtering okay\n\n")
+    with open(hwefile, "r") as hwe:
+        for line in hwe:
+            if "variants loaded from .bim file" in line:
+                start_snps = line.split()[0]
+                print("HWE_Filtering")
+                print("StartingSNPs:\t", start_snps)
+            if "removed due to Hardy-Weinberg exact test." in line:
+                rem_snps = line.split()[1]
+                end_snps = int(start_snps) - int(rem_snps)
+                proportion = int(rem_snps)/int(start_snps)
+                percent = str(round(int(rem_snps)/int(start_snps)*100, 3))
+                print("RemovedSNPs:\t" + rem_snps)
+                print("EndSNPs:\t" + str(end_snps))
+                print("PercentSNPRemoved:\t" + percent + "%")
+                #print(proportion)
+                if proportion > 0.1:
+                    print("!!HOLY SHIT WE DROPPED TOO MUCH!!\n\n")
+                else:
+                    print("HWE Filtering okay\n\n")
